9a6a84cea2df191547985bce542a17f4b437b6b6
Nasanjargal <nasaa1997@gmail.com>
2022-03-21 00:57:28 -0500

    added seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta name="og:title" content="Nasanjargal Binderiya" />
+        <meta name="og:description" content="Nasaa's personal website" />
+        <meta
+          name="og:keywords"
+          content="Nasanjargal.com, Nasanjargal Binderiya, Nasanjargal, Nasaa.com, Nasaa"
+        />
+        <meta
+          property="og:image"
+          content="https://www.nasanjargal.com/image.png"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+      </Head>
       <body>
         <Main />
         <NextScript />
