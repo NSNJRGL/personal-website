71c8430bbf9735cff51cb5f6447fbeaa9ee05abc
nasanjargal-Binderiya <nasanjargal.binderiya@thermofisher.com>
2024-04-05 12:45:50 -0500

    added analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -21,6 +22,18 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-B1LF5T79X0"
+        />
+        <Script id="google-analytics">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-B1LF5T79X0');
+        `}
+        </Script>
       </body>
     </Html>
   );
